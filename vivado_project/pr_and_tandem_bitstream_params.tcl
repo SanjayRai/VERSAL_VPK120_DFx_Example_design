b3dffedb1f02caac38306f61324f4038158195af
SanjayRai <sanjay.d.rai@gmail.com>
2022-09-25 12:41:50 -0700

    Tandem params file

---
+++ vivado_project/pr_and_tandem_bitstream_params.tcl
@@ -0,0 +1,11 @@
+# PR Enablement
+set_param hd.enablePR 2591
+set_param bitstream.enablePR 8519
+
+# PC Enablement (Tandem)
+set_param hd.enablePC 3475
+set_param bitstream.enablePC 7635
+
+# PR + PC Enablement (Tandem)
+set_param hd.enableTandemPR 6379
+set_param bitstream.enableTandemPR 4731
